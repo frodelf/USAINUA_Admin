111358f604768a835d923b541ea8cad1207b37c3
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-04-28 23:47:02 +0300

    second commit

--- src/main/java/com/avadamedia/USAINUA_Admin/UsainuaApplication.java
+++ src/main/java/com/avadamedia/USAINUA_Admin/UsainuaApplication.java
@@ -3,12 +3,18 @@ package com.avadamedia.USAINUA_Admin;
 import com.avadamedia.USAINUA_Admin.entity.Users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UsainuaApplication {
+public class UsainuaApplication extends SpringBootServletInitializer {
 	public static Users user;
 	public static void main(String[] args) {
 		SpringApplication.run(UsainuaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UsainuaApplication.class);
+	}
 }
