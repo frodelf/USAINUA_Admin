48cf93956220af0faea51bcd1e8a152621206ce9
Denis Derkach <derkach.denis@lll.kpi.ua>
2023-07-07 19:49:54 +0300

    bug fix

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,6 +1,11 @@
-document.getElementById('deleteButton').addEventListener('click', function (event) {
-    event.preventDefault();
-    if (confirm('Ви впевнені, що хочете видалити цей запис?')) {
-        document.getElementById('myForm').submit();
+document.addEventListener('DOMContentLoaded', function () {
+    var deleteButton = document.getElementById('deleteButton');
+    if (deleteButton) {
+        deleteButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            if (confirm('Ви впевнені, що хочете видалити цей запис?')) {
+                document.getElementById('myForm').submit();
+            }
+        });
     }
 });
